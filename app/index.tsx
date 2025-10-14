af73cb9abf77aa91be6aa53d2ec69e2b513d61eb
Xander Mulligan <xander.i.mulligan@gmail.com>
2025-10-14 14:10:11 -0600

    Expo project

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import Posts from "@/components/Posts";
+import { View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Posts/>
+    </View>
+  );
+}
---
+++ components/Posts.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View} from 'react-native'
+import React from 'react'
+
+function Posts() {
+    <View>
+        
+    </View>
+}
+
+export default Posts
+
+const styles = StyleSheet.create({})
