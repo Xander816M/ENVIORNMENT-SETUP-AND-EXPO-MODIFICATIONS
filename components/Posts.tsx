b91c134a90e8dc3698e2e2f97cb9376620c41f7f
Abdullah <abdullahdauda55@gmail.com>
2025-10-18 14:40:57 -0600

    Fix bottom bar + heading + navigation bar merge conflicts

--- components/Posts.tsx
+++ components/Posts.tsx
@@ -1,8 +1,9 @@
 import { StyleSheet, Text, View, Image, TouchableOpacity, Alert } from 'react-native'
 import React, {useState} from 'react'
-import { Button } from '@react-navigation/elements'
 
+// style={styles.container}
 const Posts = () => {
+    const[pageSelected, setPageSelected] = useState(false)
 
     const handleHomePress = () => {
         Alert.alert("Home Button", "You tapped the Home icon!");
@@ -30,67 +31,65 @@ const Posts = () => {
 
 
     return (
-        <View style={styles.container}>
-            <View style={styles.bottomBar}>
+        
+            
                 
-                
-                <TouchableOpacity onPress={handleHomePress}>
-                    <Image source={require("../assets/images/home.png")} style={styles.bottomButton} />
-                </TouchableOpacity>
+        <View >
+            <View>
+                <View style={styles.heading}>
+                    <Image source={{uri: "https://randomuser.me/api/portraits/men/52.jpg",
+                    }}
+                    style={styles.profileImage}
+                    />
+                    <Image source={{uri: "https://upload.wikimedia.org/wikipedia/commons/0/09/X_%28formerly_Twitter%29_logo_late_2025.svg",}}
+                    style={styles.logo}/>
+                    <Image source={{uri: "https://upload.wikimedia.org/wikipedia/commons/8/80/Codicons_%E2%80%93_settings-gear.svg",}}
+                    style={styles.settings}/>
+                </View>
 
-                <TouchableOpacity onPress={handleSearchPress}>
-                    <Image source={require("../assets/images/search.png")} style={styles.bottomButton} />
-                </TouchableOpacity>
+                <View style={styles.navigationBar}>
+                    <TouchableOpacity onPress={() => setPageSelected(false)} style={pageSelected === false&& {borderBottomColor: 'rgba(90, 143, 249, 1)', borderBottomWidth: 3, borderBottomLeftRadius: 2, borderBottomRightRadius: 2, borderTopLeftRadius:3}}> 
+                        <Text style={[styles.tab,pageSelected === true&& {color:'rgba(100, 100, 101, 1)'}]}>For you</Text>
+                    </TouchableOpacity>
 
-                <TouchableOpacity onPress={handleGrokPress}>
-                    <Image source={require("../assets/images/grok.png")} style={styles.bottomButton} />
-                </TouchableOpacity>
-
-                <TouchableOpacity onPress={handleNotificationsPress}>
-                    <Image source={require("../assets/images/notifications.png")} style={styles.bottomButton} />
-                </TouchableOpacity>
-
-                <TouchableOpacity onPress={handleInboxPress}>
-                    <Image source={require("../assets/images/inbox.png")} style={styles.bottomButton} />
-                </TouchableOpacity>
-
-                <TouchableOpacity style={styles.alertButton} onPress={handleAlertPress}>
-                    <Text style={styles.alertText}>Alert</Text>
-                </TouchableOpacity>
+                    <TouchableOpacity onPress={() => setPageSelected(true)} style={pageSelected === true&& {borderBottomColor: 'rgba(90, 143, 249, 1)', borderBottomWidth: 3, borderBottomLeftRadius: 2, borderBottomRightRadius: 2, borderTopLeftRadius:3}}> 
+                        <Text style={[styles.tab,pageSelected === false&& {color:'rgba(100, 100, 101, 1)'}]}>Following</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
-        </View>
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'
-import React, {useState} from 'react'
 
-const Posts = () => {
-  const[pageSelected, setPageSelected] = useState(false)
-  return (
-    <View>
-      <View>
-        <View style={styles.heading}>
-          <Image source={{uri: "https://randomuser.me/api/portraits/men/52.jpg",
-          }}
-          style={styles.profileImage}
-          />
-          <Image source={{uri: "https://upload.wikimedia.org/wikipedia/commons/0/09/X_%28formerly_Twitter%29_logo_late_2025.svg",}}
-          style={styles.logo}/>
-          <Image source={{uri: "https://upload.wikimedia.org/wikipedia/commons/8/80/Codicons_%E2%80%93_settings-gear.svg",}}
-          style={styles.settings}/>
-        </View>
-        <View style={styles.navigationBar}>
-          <TouchableOpacity onPress={() => setPageSelected(false)} style={pageSelected === false&& {borderBottomColor: 'rgba(90, 143, 249, 1)', borderBottomWidth: 3, borderBottomLeftRadius: 2, borderBottomRightRadius: 2, borderTopLeftRadius:3}}> 
-            <Text style={[styles.tab,pageSelected === true&& {color:'rgba(100, 100, 101, 1)'}]}>For you</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => setPageSelected(true)} style={pageSelected === true&& {borderBottomColor: 'rgba(90, 143, 249, 1)', borderBottomWidth: 3, borderBottomLeftRadius: 2, borderBottomRightRadius: 2, borderTopLeftRadius:3}}> 
-            <Text style={[styles.tab,pageSelected === false&& {color:'rgba(100, 100, 101, 1)'}]}>Following</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
+            <View >       
+                <View style={styles.bottomBar}>
 
-    </View>
-  )
+                    <TouchableOpacity onPress={handleHomePress}>
+                        <Image source={require("../assets/images/home.png")} style={styles.bottomButton} />
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={handleSearchPress}>
+                        <Image source={require("../assets/images/search.png")} style={styles.bottomButton} />
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={handleGrokPress}>
+                        <Image source={require("../assets/images/grok.png")} style={styles.bottomButton} />
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={handleNotificationsPress}>
+                        <Image source={require("../assets/images/notifications.png")} style={styles.bottomButton} />
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={handleInboxPress}>
+                        <Image source={require("../assets/images/inbox.png")} style={styles.bottomButton} />
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.alertButton} onPress={handleAlertPress}>
+                        <Text style={styles.alertText}>Alert</Text>
+                    </TouchableOpacity>
+                </View>
+            </View> 
+        </View>
+    )
+
 }
-
 export default Posts
 
 const styles = StyleSheet.create({
@@ -100,11 +99,12 @@ const styles = StyleSheet.create({
 
     bottomBar: {
         position: "absolute",
-        bottom:-1000,
+        bottom:-870,
         left: 0,
         right: 0,
         height:70,
         flexDirection: "row",
+        flex:1,
         justifyContent: "space-between",
         alignItems: "center",
         borderTopWidth: 2.5,
@@ -135,39 +135,37 @@ const styles = StyleSheet.create({
     alertText:{
         fontWeight:"bold",
         fontSize:18,
+    },
+
+    profileImage:{
+        width:30,
+        height: 30,
+        borderRadius: 20,
+    },
+    logo:{
+        width: 30,
+        height: 30
+    },
+    settings:{
+        width: 30,
+        height: 30
+    },
+    heading:{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginTop: 25,
+        padding: 15,
+        paddingBottom: 30
+    },
+    navigationBar:{
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        borderBottomWidth: 1,
+        borderBottomColor: 'rgba(196, 196, 198, 1)',
+    },
+    tab:{
+        paddingBottom: 5.5,
+        paddingLeft:57,
+        paddingRight:57
     }
-})
-  
-  
-  profileImage:{
-    width:30,
-    height: 30,
-    borderRadius: 20,
-  },
-  logo:{
-    width: 30,
-    height: 30
-  },
-  settings:{
-    width: 30,
-    height: 30
-  },
-  heading:{
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginTop: 25,
-    padding: 15,
-    paddingBottom: 30
-  },
-  navigationBar:{
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    borderBottomWidth: 1,
-    borderBottomColor: 'rgba(196, 196, 198, 1)',
-  },
-  tab:{
-    paddingBottom: 5.5,
-    paddingLeft:57,
-    paddingRight:57
-  }
 })
